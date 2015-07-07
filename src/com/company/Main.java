ac97b83e44707e92e11e70f5267aca474f379cba
SomovEvgeny <som.evgeny@mail.ru>
2015-07-07 18:56:35 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        while (scanner.hasNextLine()) {
+            command = scanner.nextLine();
+            System.out.println(test(command));
+        }
+    }
+    public static boolean test (String str){
+        Pattern pattern = Pattern.compile("^addAmount\\([\\d]{1,5},[\\d]{1,5}\\)$");
+        Matcher matcher = pattern.matcher(str);
+        return  matcher.matches();
+    }
+
+
+
+}
